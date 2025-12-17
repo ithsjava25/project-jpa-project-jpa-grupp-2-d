30c1788bf7d61e713e78866ee3edbed48477ecd4
Adam Ottosson <addeecsgo@gmail.com>
2025-12-17 21:22:41 +0100

    adds a comment

--- src/main/java/org/example/DTO/TopRatedResponseDTO.java
+++ src/main/java/org/example/DTO/TopRatedResponseDTO.java
@@ -3,7 +3,7 @@ package org.example.DTO;
 import java.util.List;
 
 public class TopRatedResponseDTO {
-    // This is what we get in response from the API
+    // This is what we get in response from the API. API response is a wrapper-object.
     public int page;
     public List<MovieDTO> results;
     public int total_pages;
