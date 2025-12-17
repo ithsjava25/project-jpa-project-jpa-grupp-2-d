a34a97baaf6bb8ac558790efccdc72a9d01e531f
Adam Ottosson <addeecsgo@gmail.com>
2025-12-17 21:22:02 +0100

    adds MovieDetailsDTO

---
+++ src/main/java/org/example/DTO/MovieDetailsDTO.java
@@ -0,0 +1,13 @@
+package org.example.DTO;
+
+public class MovieDetailsDTO {
+    public int id;
+    public String title;
+    public String overview;
+    public String release_date;
+    public int runtime;
+    public double vote_average;
+    public String poster_path;
+
+    // public List<GenreDTO> genres; Should we have this?
+}
