5df26682be1f4977e88d128f0ffa5adee9745046
Adam Ottosson <addeecsgo@gmail.com>
2025-12-17 21:21:36 +0100

    adds CrewDTO

---
+++ src/main/java/org/example/DTO/CrewDTO.java
@@ -0,0 +1,8 @@
+package org.example.DTO;
+
+public class CrewDTO {
+    public int id;
+    public String name;
+    public String profile_path;
+    public String job; // example: Director, Assistant Director, Producer etc.
+}
