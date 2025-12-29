2360028d5277a5963c12ff70575d24f4e813536f
Adam Ottosson <addeecsgo@gmail.com>
2025-12-29 19:54:03 +0100

    Add inTransactionResult utility method for returning values

--- src/main/java/org/example/util/JPAUtil.java
+++ src/main/java/org/example/util/JPAUtil.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class JPAUtil {
 
@@ -31,6 +32,24 @@ public class JPAUtil {
             tx.begin();
             work.accept(em);
             tx.commit();
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+
+    public static <T> T inTransactionResult(Function<EntityManager, T> work) {
+        EntityManager em = getEntityManager();
+        try {
+            var tx = em.getTransaction();
+            tx.begin();
+            T result = work.apply(em);
+            tx.commit();
+            return result;
         } catch (Exception e) {
             if (em.getTransaction().isActive()) {
                 em.getTransaction().rollback();
