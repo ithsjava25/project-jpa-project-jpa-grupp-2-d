f21ac165514cf9188a216dc827f3765d5b65123c
Adam Ottosson <addeecsgo@gmail.com>
2025-12-31 14:50:33 +0100

    adds JavaFX HostServices for external link handling

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,16 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.application.HostServices;
 import javafx.stage.Stage;
 import org.example.ui.MainApp;
 import org.example.util.JPAUtil;
 
 public class App extends Application {
-
+    public static HostServices HOST_SERVICES;
     @Override
     public void start(Stage primaryStage) {
+        HOST_SERVICES = getHostServices();
         MainApp mainApp = new MainApp();
         mainApp.start(primaryStage);
     }
