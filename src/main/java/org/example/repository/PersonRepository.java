56045bbb8971557493cc837485ae0c80225e4589
Adam Ottosson <addeecsgo@gmail.com>
2026-01-02 21:55:06 +0100

    Add EntityManager-aware save and findByName methods to avoid nested transactions

--- src/main/java/org/example/repository/PersonRepository.java
+++ src/main/java/org/example/repository/PersonRepository.java
@@ -1,5 +1,6 @@
 package org.example.repository;
 
+import jakarta.persistence.EntityManager;
 import org.example.movie.entity.Person;
 
 import java.util.List;
@@ -9,9 +10,14 @@ public interface PersonRepository {
 
     Person save(Person person);
 
+    // use when we already are in a transaction and have EntityManager.
+    Person save(EntityManager em, Person person);
+
     Optional<Person> findById(Long id);
 
     Optional<Person> findByName(String name);
+
+    Optional<Person> findByName(EntityManager em, String name);
 
     List<Person> findAll();
 
