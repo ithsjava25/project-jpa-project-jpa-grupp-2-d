8dfb5c1068e8b7f43ad4d59c952c1da005a104ca
Adam Ottosson <addeecsgo@gmail.com>
2026-01-12 12:04:45 +0100

    adds favorite repository

---
+++ src/main/java/org/example/repository/FavoriteRepositoryImpl.java
@@ -0,0 +1,83 @@
+package org.example.repository;
+
+import jakarta.persistence.EntityManager;
+import org.example.movie.entity.Favorite;
+import org.example.util.JPAUtil;
+
+import java.util.List;
+import java.util.Optional;
+
+public class FavoriteRepositoryImpl implements FavoriteRepository{
+    @Override
+    public Favorite save(Favorite favorite) {
+        JPAUtil.inTransaction(em -> em.persist(favorite));
+        return favorite;
+    }
+
+    @Override
+    public void delete(Favorite favorite) {
+        JPAUtil.inTransaction(em -> {
+            Favorite managed = em.merge(favorite);
+            em.remove(managed);
+        });
+    }
+
+    @Override
+    public Optional<Favorite> findByMovieId(Long movieId) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            return em.createQuery(
+                    "SELECT f FROM Favorite f WHERE f.movie.id = :movieId",
+                    Favorite.class
+                )
+                .setParameter("movieId", movieId)
+                .getResultStream()
+                .findFirst();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Favorite> findAll() {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            return em.createQuery(
+                "SELECT f FROM Favorite f",
+                Favorite.class
+            ).getResultList();
+
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public long count() {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            return em.createQuery(
+                "SELECT COUNT(f) FROM Favorite f",
+                Long.class
+            ).getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public Optional<Favorite> findByTmdbId(int tmdbId) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            return em.createQuery(
+                    "SELECT f FROM Favorite f WHERE f.tmdbId = :tmdbId",
+                    Favorite.class
+                )
+                .setParameter("tmdbId", tmdbId)
+                .getResultStream()
+                .findFirst();
+        } finally {
+            em.close();
+        }
+    }
+}
